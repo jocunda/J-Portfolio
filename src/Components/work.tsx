83a2e8df0b492ff485632afc31a6d5425dbdd87b
jocunda <silviawanshan88@gmail.com>
2022-09-25 19:55:22 +0800

    Feat:mywork

--- src/Components/work.tsx
+++ src/Components/work.tsx
@@ -12,36 +12,28 @@ import cx from 'classnames';
 export default function Work() {
 
     const initialState = Math.floor(WORKIMAGES.length / 2);
+    const containerState = -45
     const [work, setWork] = useState<number>(initialState);
+    const [container, setContainer] = useState<number>(containerState);
 
 
 
     function nextSlide() {
         setWork(work === WORKIMAGES.length - 1 ? 0 : work + 1)
+        setContainer(work === WORKIMAGES.length - 1 ? containerState : container - 7)
     };
     function prevSlide() {
         setWork(work === 0 ? WORKIMAGES.length - 1 : work - 1)
+        setContainer(work === 0 ? WORKIMAGES.length - containerState : container + 7)
     };
-
-    function slickimage(key: number) {
-        if (key === work) {
-            return cx(styles.workimage, styles.imageactive)
-        } else if (key === work - 1) {
-            return cx(styles.workimage, styles.clippedleft)
-        } else if (key === work + 1) {
-            return cx(styles.workimage, styles.clippedright)
-        }
-        else return styles.workimage
-    }
-
 
     return <>
         <h2>My Works</h2>
         <div className={styles.workimageSlick}>
-            <div className={styles.workimageContainer}>
+            <div className={styles.workimageContainer} style={{ transform: `translate3d(calc(${container}% + 2vmin), 0px, 0px)` }}>
                 {WORKIMAGES.map(({ key, image, text }, index) =>
                     <div key={index} className={styles.workimagediv}>
-                        <img src={image} alt={text} className={slickimage(key)} />
+                        <img src={image} alt={text} className={key === work ? cx(styles.workimage, styles.imageactive) : styles.workimage} />
                     </div>
                 )}
             </div>
