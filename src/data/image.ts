a4b6899760ea23fc6e2c86665a4b0a3dc449f5de
jocunda <silviawanshan88@gmail.com>
2022-10-18 20:28:42 +0800

    Add work

--- src/data/image.ts
+++ src/data/image.ts
@@ -1,6 +1,8 @@
 //import image
 import price from '../assets/workimage/pricegrid.jpg'
 import price1 from '../assets/workimage/pricegrid1.jpg'
+import login from '../assets/workimage/login.jpg'
+import earth from '../assets/workimage/earth.jpg'
 
 const WORKIMAGES: WorkImage[] = [
     {
@@ -35,22 +37,22 @@ const WORKIMAGES: WorkImage[] = [
         image: price,
         image1: price1,
         title: "Price Grid Component",
-        text: "Challenge from frontend mentor, to build out the pricing component to the design provided. This smaller challenge is a start for beginner or newbie, which can practice using CSS feature such as flexbox and grid. Component also must have optimal layout for device's screen size.",
+        text: "Newbie challenge from frontend mentor, to build out the pricing component to the design provided. This smaller challenge is a start for beginner or newbie, which can practice using CSS feature such as flexbox and grid. Component also must have optimal layout for device's screen size.",
         framework: "React 18.2.0 CRA",
         link: "https://github.com/jocunda/FM-price_grid",
     },
     {
         key: 4,
-        image: "https://picsum.photos/id/1045/800/600",
+        image: earth,
         image1: "",
         title: "3D Earth",
-        text: "Bla bla bla bla bla",
+        text: "Web 3D project that using library such as react-three/drei, react-three/fiber. Simulate earth's orbiting the sun in star galaxy, which surface will have dark and bright side. This earth 3D can controlled by zoom and rotate option. Surface material use some properties with metalness and roughness to make it realistic. Cloudy map also set as upper layer to wrap earth surface with cloud.",
         framework: "React 17.0.2 CRA",
         link: "https://jocunda.github.io/React-Globe/",
     },
     {
         key: 5,
-        image: "https://picsum.photos/id/1047/800/600",
+        image: "https://picsum.photos/id/1053/800/600",
         image1: "",
         title: "Login",
         text: "Bla bla bla bla bla",
