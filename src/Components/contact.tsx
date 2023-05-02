392aa51eeaa83d5985723aeaab1ff9eafc06525e
jocunda <silviawanshan88@gmail.com>
2023-05-02 23:29:15 +0800

    add leetcode symbol

--- src/Components/contact.tsx
+++ src/Components/contact.tsx
@@ -2,6 +2,7 @@
 import { ImLinkedin2 } from 'react-icons/im'
 import { AiFillGithub, AiFillYoutube } from 'react-icons/ai'
 import { FiMail } from 'react-icons/fi'
+import { SiLeetcode } from 'react-icons/si'
 
 //styles
 import styles from './component.module.scss'
@@ -14,6 +15,7 @@ export default function Contact() {
             <div className={styles.iconContainer}>
                 <a href="https://linkedin.com/in/silviajocunda" target="_blank"><ImLinkedin2 /></a>
                 <a href="https://github.com/jocunda" target="_blank"><AiFillGithub /></a>
+                <a href="https://leetcode.com/Jocunda/" target="_blank"><SiLeetcode /></a>
                 <a href="mailto:silviawanshan88@gmail.com" target="_blank" title="silviawanshan88@gmail.com"><FiMail /></a>
             </div>
         </div>
